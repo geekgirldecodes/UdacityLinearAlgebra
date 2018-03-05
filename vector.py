72d1ddf5169061998dd12c21cbe06eb9c49d82d9
void* <26856838+geekgirldecodes@users.noreply.github.com>
2018-03-05 13:20:17 -0800

    vector section 8 complete

--- vector.py
+++ vector.py
@@ -5,12 +5,23 @@
 # Refer : https://classroom.udacity.com/courses/ud953
 # Author : @geekgirldecodes
 
+# Imports
+from math import sqrt, acos, pi
+from decimal import Decimal, getcontext
+getcontext.prec = 30
+
 class Vector(object):
+
+    ATTEMPTING_ZERO_VECTOR_NORMALIZE_ERROR = "Zero vector cannot be normalized"
+    
     def __init__(self, coordinates):
         try:
             if not coordinates:
                 raise ValueError
-            self.coordinates = tuple(coordinates)
+            # All co-ordinates are "Decimal" instead of float/integer
+            # This is needed for numerical precision to help with
+            # acos computation (refer : dot product)
+            self.coordinates = tuple([Decimal(x) for x in coordinates])
             self.dimension = len(coordinates)
 
         except ValueError:
@@ -47,7 +58,7 @@ class Vector(object):
 
     def scalar_multiply(self, s):
         #find the scalar product of vetcor and scalar passed
-        s_prod = [s*x for x in self.coordinates]
+        s_prod = [Decimal(s)*x for x in self.coordinates]
         return Vector(s_prod)
 
     
@@ -59,12 +70,8 @@ class Vector(object):
     def magnitude(self):
         # Finds magnitude of the given vector
         # magnitude(V) = sqrt(V1^2 +V2^2 + ...Vn^2)
-        v_mag = 0
-        for x in self.coordinates:
-            print x
-            v_mag = x**2+v_mag #** is operator for : raised to the power of
-        return (v_mag**0.5)
-
+        v_sqr = [x**2 for x in self.coordinates] #** is operator for : raised to the power of
+        return Decimal(sqrt(sum(v_sqr)))
 
     def direction(self):
         # Finds direction of the given vector
@@ -73,5 +80,43 @@ class Vector(object):
         v_dir = self.scalar_multiply(s)
         return v_dir
         
-        
-        
+###########################
+# quiz 03
+# Inner/Dot Product and
+# angle
+###########################        
+
+    def normalize(self):
+        try:
+            mag = self.magnitude()
+            return self.scalar_multiply(Decimal('1.0')/mag)
+        except ZeroDivisionError:
+            raise Exception(self.ATTEMPTING_ZERO_VECTOR_NORMALIZE_ERROR)
+
+    def dot_product(self,v):
+        # Compute the inner product between
+        # two vetcors
+        return sum([x*y for x,y in zip(self.coordinates, v.coordinates)])
+            
+    def angle_between(self,v, in_degree=False):
+        # Compute the angle between two vectors
+        # angle_in_radians = arccos(V1/magnitude(V1) . V2/(magnitude(V2))
+
+        try:
+            V1 = self.normalize()
+            V2 = v.normalize()
+            # use acos() from Math library 
+            angle_in_radians = acos(V1.dot_product(V2))
+
+            if (in_degree):
+                degrees_per_rad = 180./pi
+                return angle_in_radians * degrees_per_rad
+            else:
+                return angle_in_radians
+
+        except Exception as e:
+            if str(e) == self.ATTEMPTING_ZERO_VECTOR_NORMALIZE_ERROR:
+                raise Exception("cannot compute angle with Zero vector")
+            else:
+                raise e                
+            
